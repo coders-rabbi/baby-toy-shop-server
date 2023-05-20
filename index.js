09ce4d51cf617996ba7d8ed4d99ae8c337fb4e30
coders rabbi <pcrabbi2020@gmail.com>
2023-05-20 11:55:17 +0600

    data retrive from the mongodb using category

--- index.js
+++ index.js
@@ -36,10 +36,26 @@ async function run() {
 
         app.get('/alltoys/:id', async (req, res) => {
             const id = req.params.id;
+            // console.log(id);
             const query = { _id: new ObjectId(id) }
             const result = await alltoysCollection.findOne(query);
             res.send(result);
         })
+
+
+
+        app.get("/category/:text", async (req, res) => {
+            console.log(req.params.text);
+            const  text = req.params.text;
+            const query = {category: text};
+            const result = await alltoysCollection.find(query).toArray();
+            res.send(result);
+        });
+
+
+       
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
