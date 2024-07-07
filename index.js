0802ae0835e4b8b2f6be122ac335e9429de1f9fa
coders-rabbi <pcrabbi2020@gmail.com>
2024-07-07 16:45:08 +0600

    user filter by email route work perfectly

--- index.js
+++ index.js
@@ -40,6 +40,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", async (req, res) => {
+      const result = await allUsers.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      console.log(email);
+      const result = await allUsers.findOne(query);
+      res.send(result);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
 
