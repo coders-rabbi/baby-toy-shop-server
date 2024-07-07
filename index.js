a2f3b4150436d90aac37dac606121569ce8f9b54
coders-rabbi <pcrabbi2020@gmail.com>
2024-07-07 23:39:15 +0600

    user data fetch by user email

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookings/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      console.log(email);
+      const result = await allBookings.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/booking", async (req, res) => {
       const booking = req.body;
 
